494de5448ed7f20e0cab47eab1834eaa647c3793
hyejiseo-dev <dianna537@gmail.com>
2021-07-28 16:43:10 +0900

    task page ui database setting complete

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:startapp/screens/taskpage.dart';
 import 'package:startapp/widget.dart';
 
 class HomePage extends StatefulWidget {
@@ -27,16 +28,19 @@ class _HomePageState extends State<HomePage> {
                   ),
                   Container(margin: EdgeInsets.only(bottom: 20.0)),
                   Expanded(
-                    child: ListView(
-                      children: [
-                        TaskCardWidget(
-                          title: "Get Started",
-                          des: 'Hello! this is a default memo for you',
-                        ),
-                        TaskCardWidget(),
-                        TaskCardWidget(),
-                        TaskCardWidget(),
-                      ],
+                    child: ScrollConfiguration(
+                      behavior: NoGlowBehaviour(),
+                      child: ListView(
+                        children: [
+                          TaskCardWidget(
+                            title: "Get Started",
+                            des: 'Hello! this is a default memo for you',
+                          ),
+                          TaskCardWidget(),
+                          TaskCardWidget(),
+                          TaskCardWidget(),
+                        ],
+                      ),
                     ),
                   )
                 ],
@@ -45,14 +49,24 @@ class _HomePageState extends State<HomePage> {
               Positioned(
                 bottom: 0.0,
                 right: 0.0,
-                child: Container(
-                  width: 60.0,
-                  height: 60.0,
-                  decoration: BoxDecoration(
-                      color: Color(0xFF7349FE),
-                      borderRadius: BorderRadius.circular(20.0)),
-                  child: Image(
-                    image: AssetImage('assets/add_icon.png'),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => TaskPage()));
+                  },
+                  child: Container(
+                    width: 60.0,
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [Color(0xFF7349FE), Color(0xFF643FDB)],
+                          begin: Alignment(0.0,-1.0),
+                          end : Alignment(0.0,1.0)
+                        ),
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: Image(
+                      image: AssetImage('assets/add_icon.png'),
+                    ),
                   ),
                 ),
               )
--- lib/screens/taskpage.dart
+++ lib/screens/taskpage.dart
@@ -1,2 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:startapp/widget.dart';
 
+class TaskPage extends StatefulWidget {
+  const TaskPage({Key? key}) : super(key: key);
+
+  @override
+  _TaskpageState createState() => _TaskpageState();
+}
+
+class _TaskpageState extends State<TaskPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Stack(
+            children: [
+              Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 24.0,
+                    bottom: 6.0
+                  ),
+                  child: Row(
+                    children: [
+                      InkWell(
+                        onTap: (){
+                          Navigator.pop(context);
+                         // print("Clicked Back Button...");
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(24.0),
+                          child: Image(
+                            image: AssetImage('assets/back_arrow_icon.png'),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: TextField(
+                          onSubmitted: (value){
+                            print('value : $value');
+                          },
+                          decoration: InputDecoration(
+                            hintText: "Enter Task Title..",
+                            border : InputBorder.none
+                          ),
+                          style: TextStyle(
+                            fontSize: 26.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF211511)
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 12.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Description for the task...",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 24.0
+                      )
+                    ),
+                  ),
+                ),
+                TodoWidget(text: 'Create your First Task',isDone: true,),
+                TodoWidget(text: 'Create your First todo as well',isDone: true),
+                TodoWidget(isDone: false),
+                TodoWidget(isDone: false),
+              ],
+            ),
+              Positioned(
+                bottom: 24.0,
+                right: 24.0,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => TaskPage()));
+                  },
+                  child: Container(
+                    width: 60.0,
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                        color: Color(0xFFFE3577),
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: Image(
+                      image: AssetImage('assets/delete_icon.png'),
+                    ),
+                  ),
+                ),
+              )
+          ]
+          ),
+        ),
+      ),
+    );
+  }
+}
